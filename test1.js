8cb8c46ddba8d8e67c0a37bb6e5c80a55cd516c8
Dmitronish <dmitronish@mail.ru>
2022-01-07 23:25:02 +0300

    first commit

---
+++ test1.js
@@ -0,0 +1,57 @@
+"Use strict";
+
+// function showMessage() {
+//     let message = "Привет, я JavaScript!"; // локальная переменная
+
+//     document.write( message );
+//   }
+
+//   showMessage(); // Привет, я JavaScript!
+
+// function my_function(n) {
+//     if (n==0) {
+//         return 0 ; 
+//     }else if (n==1) {
+//         return 1; 
+//     }else{
+//         return my_function(n-1) + " " + n;
+
+//     }
+
+// }
+// document.write(my_function(3));
+
+// обьекты
+// const obj = {
+//     name: "Dima",
+//     age: 39,
+//     isMaried: true
+// };
+// console.log(obj["name"]);
+// // массив
+// let arr = ["aple.jpg", "nut.gif", 6, "lemon.bmp", {}, []];
+// console.log(arr[1]);
+
+// взаимодействие с пользователем
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+// document.write(answers);
+
+// интерполяция в js
+// const user = "lesha";
+// console.log("Привет" + " " + user);
+
+// const names = "Dima";
+// console.log(`Привет, ${names}`);
+
+
+// операторы
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+
