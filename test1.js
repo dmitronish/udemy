93ac33f126c658f9dee2e1b053121b3894aad577
Dmitronish <dmitronish@mail.ru>
2022-01-09 01:24:08 +0300

    практика 2

--- test1.js
+++ test1.js
@@ -55,7 +55,142 @@
 // console.log(incr);
 // console.log(decr);
 
-// консольное приложение
+// консольное приложение практика
+// const numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+// const personalMuvieDb = {
+//     count: numberOfFilms,
+//     muvies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+// const lastSeeMuvie = prompt("Последний из просмотренных фильмов?", ""),
+//     ocenka = prompt("На сколько ецените его?", ""),
+//     lastSeeMuvie2 = prompt("Последний из просмотренных фильмов?", ""),
+//     ocenka2 = prompt("На сколько ецените его?", "");
+
+// personalMuvieDb.muvies[lastSeeMuvie] = ocenka;
+// personalMuvieDb.muvies[lastSeeMuvie2] = ocenka2;
+// console.log(personalMuvieDb);
+
+// условные операторы
+// тернарный оператор
+// const num = 150;
+// (num === 50) ? console.log("ok!") : console.log("error!");
+
+// const num = 50;
+// switch (num) {
+//     case 49:
+//         console.log("неверно");
+//         break;
+//     case 100:
+//         console.log("неверно");
+//         break;
+//     case 50:
+//         console.log("dthyj");
+//         break;
+//     default:
+//         console.log("не в этот раз");
+// }
+
+
+// циклы
+// let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 50;
+// do {
+//     console.log(num);
+//     num++;
+// } 
+// while (num < 55);
+
+// let num = 50;
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// практика 2
+
+// const numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+// const personalMuvieDb = {
+//     count: numberOfFilms,
+//     muvies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const lastSeeMuvie = prompt("Последний из просмотренных фильмов?", ""),
+//         ocenka = prompt("На сколько ецените его?", "");
+//     if (lastSeeMuvie != null && ocenka != null &&
+//         lastSeeMuvie != "" && ocenka != "" && lastSeeMuvie.length < 50) {
+//         personalMuvieDb.muvies[lastSeeMuvie] = ocenka;
+//         console.log("done!");
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+// }
+// if (personalMuvieDb.count < 10) {
+//     console.log("просотрено довольно мало фильмов");
+// } else if (personalMuvieDb.count >= 10 && personalMuvieDb.count < 30) {
+//     console.log("вы класический зритель");
+// } else if (personalMuvieDb.count >= 30) {
+//     console.log("вы киноман!");
+// } else {
+//     console.log("произошла ошибка");
+// }
+// console.log(personalMuvieDb);
+
+// const numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+// const personalMuvieDb = {
+//     count: numberOfFilms,
+//     muvies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+
+// let i = 0;
+// while (i < 2) {
+//     const lastSeeMuvie = prompt("Последний из просмотренных фильмов?", ""),
+//         ocenka = prompt("На сколько ецените его?", "");
+//     if (lastSeeMuvie != null && ocenka != null &&
+//         lastSeeMuvie != "" && ocenka != "" && lastSeeMuvie.length < 50) {
+//         personalMuvieDb.muvies[lastSeeMuvie] = ocenka;
+//         console.log("done!");        
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+//     i++;
+// }
+// if (personalMuvieDb.count < 10) {
+//     console.log("просотрено довольно мало фильмов");
+// } else if (personalMuvieDb.count >= 10 && personalMuvieDb.count < 30) {
+//     console.log("вы класический зритель");
+// } else if (personalMuvieDb.count >= 30) {
+//     console.log("вы киноман!");
+// } else {
+//     console.log("произошла ошибка");
+// }
+// console.log(personalMuvieDb);
+
 const numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
 const personalMuvieDb = {
     count: numberOfFilms,
@@ -64,11 +199,29 @@ const personalMuvieDb = {
     genres: [],
     private: false
 };
-const lastSeeMuvie = prompt("Последний из просмотренных фильмов?", ""),
-    ocenka = prompt("На сколько ецените его?", ""),
-    lastSeeMuvie2 = prompt("Последний из просмотренных фильмов?", ""),
-    ocenka2 = prompt("На сколько ецените его?", "");
 
-personalMuvieDb.muvies[lastSeeMuvie] = ocenka;
-personalMuvieDb.muvies[lastSeeMuvie2] = ocenka2;
+let i = 0;
+do {
+    const lastSeeMuvie = prompt("Последний из просмотренных фильмов?", ""),
+        ocenka = prompt("На сколько ецените его?", "");
+    i++;
+    if (lastSeeMuvie != null && ocenka != null &&
+        lastSeeMuvie != "" && ocenka != "" && lastSeeMuvie.length < 50) {
+        personalMuvieDb.muvies[lastSeeMuvie] = ocenka;
+        console.log("done!");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+while (i < 2);
+if (personalMuvieDb.count < 10) {
+    console.log("просотрено довольно мало фильмов");
+} else if (personalMuvieDb.count >= 10 && personalMuvieDb.count < 30) {
+    console.log("вы класический зритель");
+} else if (personalMuvieDb.count >= 30) {
+    console.log("вы киноман!");
+} else {
+    console.log("произошла ошибка");
+}
 console.log(personalMuvieDb);
