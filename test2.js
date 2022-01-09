f437370ed96898f37df8a713c260645d41303bae
Dmitronish <dmitronish@mail.ru>
2022-01-10 01:10:00 +0300

    add test2.js

---
+++ test2.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// функции
+// function showFirstMessage () {
+//     console.log("Hello world!");
+// }
+
+// showFirstMessage();
+
+// function showFirstMessage (text) {
+//     console.log(text);
+// }
+
+// showFirstMessage("Hello world!");
+
+// function calc (a, b) {
+//     return (a + b);
+// }
+// console.log(calc(1, 5));
+// console.log(calc(2, 3));
+// console.log(calc(8, 2));
+
+// function ret() {
+//     let num = 50;
+
+//     return num;
+// }
+
+// const enotherNum = ret();
+// console.log(enotherNum);
+
+// const loger = function() {
+//     console.log("Hello");
+// };
+// loger();
+
+// стрелочная функция
+
+// const calc = (a, b) => a + b;
+
+// методы и свойства строк и чисел
+
+// const str = "text";
+// const arr = [1, 2, 3];
+
+// console.log(str.length);
+// console.log(arr.length);
+
+// const str = "text";
+
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+// console.log(fruit.slice(5));
+// console.log(fruit.slice(5, 10));
+// console.log(fruit.substring(5, 10));
+// console.log(fruit.substr(5, 3));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
