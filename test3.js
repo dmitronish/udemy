648c45c9b69a0bcca883b392df57d469cd65d267
Dmitronish <dmitronish@mail.ru>
2022-01-12 01:23:34 +0300

    objects

--- test3.js
+++ test3.js
@@ -11,6 +11,7 @@ function done() {
 
 lernJs("Js", done);
 
+
 let arr = [1, 3, 5, 7, 9];
 
 function first(text, callback) {
@@ -25,3 +26,56 @@ function test() {
 }
 
 first("practic", test);
+
+function summaElemArr(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+const array = [2, 4, 6];
+let res = summaElemArr(array);
+console.log(res);
+res *= 2;
+console.log(res);
+
+// обьекты
+
+const options = {
+    name: "text",
+    width: 1024,
+    height: 1024,
+    color: {
+        border: "black",
+        bg: "red"
+    },
+    makeTest: function() {
+        console.log("test");
+    }
+};
+
+options.makeTest();
+const {border, bg} = options.color;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+// console.log(options["color"]["border"]);
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof (options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
