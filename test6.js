1c1096afcfab8ceee11fe019f8cd8face847937d
Dmitronish <dmitronish@mail.ru>
2022-01-16 18:31:44 +0300

    add test6.js

---
+++ test6.js
@@ -0,0 +1,80 @@
+"use strict";
+//динамическая типизация
+// to String
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+
+
+// console.log(typeof(5 + ""));//конкатенация (сложение со строкой)
+
+// const num = 5;
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + "px";
+// console.log(fontSize);
+
+// // to Number
+// console.log(typeof(Number("4")));
+
+// console.log(typeof(+"4"));
+
+// console.log(typeof(parseInt("15px", 10)));
+
+// let answ = +prompt('введите число', '');
+
+// to Boolean
+// 0, '', nuul, undefined, NaN - false 
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('working...');
+// }
+
+// console.log(typeof(Boolean(4)));
+
+// console.log(typeof(!!"4"));
+
+//задачи с собесов
+
+// let x = 5; 
+// alert( x++ ); //5
+
+// console.log([] + false - null + true); //"NaN"
+
+// let y = 1;
+// let x = y = 2;
+// alert(x); // 2
+
+// console.log([ ] + 1 + 2); //"12"
+
+// alert( "1"[0] ); //1
+
+// console.log(2 && 1 &&null&& 0 &&undefined);// null
+// И запинается на лжи false
+// ИЛИ запинается на правде true
+// console.log(!!( 1 && 2 ) === (1 && 2)); //false
+
+// alert( null || 2 && 3 || 4 ); //3
+
+// const a = [1, 2, 3],
+//       b = [1, 2, 3]; 
+// console.log(a==b); //false разные переменные с одинаковым значением
+
+// alert( +"Infinity" ); //Infinity
+
+// console.log("Ёжик" > "яблоко"); //false сравнение по кол-ву символов
+
+// console.log(0 || "" || 2 || undefined || true || falsе ); //2
+
+
+
+
+
